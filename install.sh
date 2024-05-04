dc81cabc86cc0f9e164a1b92c115185148b329ca
Nisamov <84858098+Nisamov@users.noreply.github.com>
2024-05-04 21:42:15 +0200

    .

--- install.sh
+++ install.sh
@@ -2,4 +2,22 @@
 # Ruta del directorio donde se encuentra el script de instalación
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
+# Var declaration
+main_file="/usr/local/sbin"
+
 # Proceso de instalacion de programa
+
+# See if program is installed
+if [[ -f $main_file/dirgestion ]]; then
+    # If exist say:
+    echo "[#] Program already exist"
+else
+    # If doesnt
+    echo "[#] Program does not exist"    
+    # Request for install
+    read -p "Do you want to install the program? [y/n]: " requestinstallation
+    if [[ $requestinstallation == "y" || $requestinstallation == "Y" ]]; then
+
+
+    fi
+fi

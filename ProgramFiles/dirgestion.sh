dcee980367bcb957be7149c7e55588ffb29cb6c0
Lobo233 <115951117+Lobo233@users.noreply.github.com>
2024-05-04 21:00:13 +0200

    .

--- ProgramFiles/dirgestion.sh
+++ ProgramFiles/dirgestion.sh
@@ -2,24 +2,61 @@
 
 # Progama por Nisamov y Lobo_233
 
+
 # Declaracion de variables
 
-#Iniciacion de progama
-echo "Que desea hacer: "
-echo "Opcion 1: Crear directorio"
-echo "Opcion 2: Eliminar directorio"
-echo "Opcion 3: Clonar directorio"
+function menu(){
+# Funcion de menu
+echo "| [Directory Gestion] |"
+echo "| [Options]:"
+echo "| Option '1' > Create directory"
+echo "| Option '2' > Remove directory"
+echo "| Option '3' > Clone directory"
 read -p "Ingrese su opcion: " opcion
+}
+
+function createdirectory(){
+    read -p "Ingrese el nombre a dar al directorio: " directoryname
+    read -p "Ingrese la ruta a crear el directorio [Ruta absoluta]: " rutedirectory
+    # Creamos el directorio en la ruta indicada por el usuario
+    sudo mkdir $directoryname $rutedirectory
+    # Ejemplo de uso:
+    # dirgestion directorioejemplo /usr/local/sbin
+}
+
+function deletedirectory(){
+    # Solicitar datos al usuario
+    read -p "Ingrese la ruta absoluta del directorio a eliminar: " deletedirectory
+    # Eliminar ruta de forma recursiva y forzosa
+    sudo rm -rf $deletedirectory
+}
+
+function clonedirectory(){
+    # Solicitar datos al usuario
+    read -p "Ingrese la ruta absoluta del directorio a clonar: " clonedirectory  
+    read -p "Ingrese la ruta de destino: " directorydestination
+    # Clonar directorio
+    sudo cp -r $clonedirectory $directorydestination
+}
+
+
+# Llamamos a la funcion menu 'menu'
+menu
 
 if [[ $opcion -eq 1 ]]; then
+    # Opcion1 > Crear directorio
     echo "Ha seleccionado la opcion 1"
+    # Llamar a la funcion createdirectory
+    createdirectory
 
 elif [[ $opcion -eq 2 ]]; then
+    # Opcion2 > Eliminar directorio
     echo "Ha seleccionado la opcion 2"
 
 elif [[ $opcion -eq 3 ]]; then
+    # Opcion3 > Clonar directorio
     echo "Ha seleccionado la opcion 3"
-
+    clonedirectory
 else
     echo "No se ha ingresado un valor valido [del 1 al 3]."
 fi
--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
 # Sacado de: https://github.com/Nisamov/autonetplan/blob/main/install.sh
 # Ruta del directorio donde se encuentra el script de instalación
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+# Proceso de instalacion de programa

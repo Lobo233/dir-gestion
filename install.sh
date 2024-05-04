4c855ec33c4e6b986e9c2b2a9f11fa948be26dc4
Lobo233 <115951117+Lobo233@users.noreply.github.com>
2024-05-04 19:58:49 +0200

    .

---
+++ install.sh
@@ -0,0 +1,3 @@
+# Sacado de: https://github.com/Nisamov/autonetplan/blob/main/install.sh
+# Ruta del directorio donde se encuentra el script de instalación
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"

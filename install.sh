c19e61da0721ad9f7c329937050fa1559fda1678
Nisamov <84858098+Nisamov@users.noreply.github.com>
2024-05-19 01:00:26 +0200

    Instalacion corregida

--- install.sh
+++ install.sh
@@ -1,6 +1,14 @@
-# Sacado de: https://github.com/Nisamov/autonetplan/blob/main/install.sh
-# Ruta del directorio donde se encuentra el script de instalación
+# No tocar - Responsable: Nisamov
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
-# Proceso de instalacion de programa
-sudo cp -r $SCRIPT_DIR/ProgramFiles/dirgestion.sh /local/usr/bin/dirgestion
+# Mientras no exista el fichero, tratar de instalarlo
+while [[ ! -f "/usr/local/sbin/dirgestion" ]]; do
+        # Copiar el fichero en la ruta
+        sudo cp "$SCRIPT_DIR/ProgramFiles/dirgestion.sh" "/usr/local/sbin/dirgestion"
+        # Si ya se ha instalado, indicar lo siguiente
+        if [[ -f "/usr/local/sbin/dirgestion" ]]; then
+            echo "Programa instalado correctamente"
+        else
+            echo "Ha ocurrido un error en la instalacion"
+        fi
+done
